a26bf07d4fda0772024c96f246cd3f00ebef97bd
tipthederiver <43258590+tipthederiver@users.noreply.github.com>
2020-02-04 14:00:16 -0500

    Update clean.py

--- clean.py
+++ clean.py
@@ -13,8 +13,8 @@ def word_counts(data):
 	return(counts)
 
 def clean_tweets(data):
-    print("Lets clean some tweets")
-    # Clean Some Tweests
-    clean_data = data
+	print("Lets clean some tweets")
+	# Clean Some Tweests
+	clean_data = data
   
-    return(clean_data)
+	return(clean_data)
